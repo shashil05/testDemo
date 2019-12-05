52d43932f3f5574e441b8d00383a5c72b711d096
ajmahesh1234 <maheshwari.ajay4@gmail.com>
2019-12-05 14:40:54 +0530

    Signed-off-by: ajmahesh1234 <maheshwari.ajay4@gmail.com>

---
+++ TestDemo1/src/main/java/TestDemo1/TestDemo1/App.java
@@ -0,0 +1,13 @@
+package TestDemo1.TestDemo1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
